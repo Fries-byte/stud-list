c788c25772e16e1462cda4e9118cce80ea8d1bb4
Fries-Byte <69freshfrenchfries@gmail.com>
2025-02-13 12:10:33 -0800

    Update src.py

--- main/src.py
+++ main/src.py
@@ -16,5 +16,15 @@ def pln(text, *args):  # Define print
             text = text.replace(placeholder, str(arg))
     print(text)
 
+def let(var_name, value):
+    if var_name.startswith("{") and var_name.endswith("}"):
+        var_name = var_name[1:-1]
+        if var_name in variables:
+            var_name = variables[var_name]
+    if value.startswith("{") and value.endswith("}"):
+        value = value[1:-1]
+        if value in variables:
+            value = variables[value]
+
 def iln(prompt):  # Define input line
     return input(prompt)
