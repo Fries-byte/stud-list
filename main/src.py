8bb67ce0a6fee12ed66d0be3d97bbbc4d7f8734c
Fries-Byte <69freshfrenchfries@gmail.com>
2025-02-13 11:45:50 -0800

    Create src.py

---
+++ main/src.py
@@ -0,0 +1,20 @@
+'''
+*   ptd::main
+*   adding input and print
+'''
+
+def pln(text, *args):  # Define print
+    text = text.replace("*n", "\n")
+    text = text.replace("*t", "\t")
+    for var_name in variables:
+        placeholder = "{" + var_name + "}"
+        if placeholder in text:
+            text = text.replace(placeholder, str(variables[var_name]))
+    for i, arg in enumerate(args):
+        placeholder = "{" + str(i) + "}"
+        if placeholder in text:
+            text = text.replace(placeholder, str(arg))
+    print(text)
+
+def iln(prompt):  # Define input line
+    return input(prompt)
